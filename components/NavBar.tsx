081e9313df669dd1e70decbda2489cbb97a0940a
heiner <heinerDevelops@gmail.com>
2025-02-07 01:31:13 +0100

    ...

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -43,9 +43,6 @@ const NavBar = () => {
           <li>
             <Link href={"/about"}>About</Link>
           </li>
-          <li>
-            <Link href={"/blog"}>Blog</Link>
-          </li>
         </ul>
         <ModeToggle />
       </div>
