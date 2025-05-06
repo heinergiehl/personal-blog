0c8d4e12138f46fbb6f3b05f412dc9377d2830f4
heiner <heinerDevelops@gmail.com>
2025-05-06 17:20:43 +0200

    prepared for github actions

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    apps: [
+        {
+            name: 'latest-portfolio',
+            cwd: '/var/www/latest-portfolio',
+            script: 'bun',
+            args: 'run start',
+            env_production: {
+                NODE_ENV: 'production',
+                PORT: process.env.APP_PORT || 3002,
+                // any other envs your app needs…
+            }
+        }
+    ]
+}
