6d9062c4020f6bf3cc85cc0bf16398fdeb0bcf50
heiner <heinerDevelops@gmail.com>
2025-05-08 15:48:59 +0200

    ...

--- components/skills/index.tsx
+++ components/skills/index.tsx
@@ -2,6 +2,7 @@ import { FaCode, FaDatabase } from "react-icons/fa"
 import { Card } from "../landingPage/card/card"
 import Spotlight from "../landingPage/card/spotlight"
 import { GitHubLogoIcon } from "@radix-ui/react-icons"
+import { GoalIcon } from "lucide-react"
 const Skills = () => {
   return (
     <section
@@ -9,7 +10,7 @@ const Skills = () => {
       className="min-h-screen bg-gradient-to-r from-white via-gray-100 to-gray-50 dark:from-gray-800 dark:via-gray-900 dark:to-black rounded-lg shadow-lg mt-16"
     >
       <h2 className="text-3xl font-bold text-slate-800 dark:text-slate-100 mb-6 text-center p-8 ">
-        Areas Of Expertise
+        Skills
       </h2>
       <Spotlight className="max-w-xl mx-auto grid gap-6 grid-cols-1 lg:grid-cols-3 items-start lg:max-w-none group lg:p-20">
         <Card
@@ -25,10 +26,10 @@ const Skills = () => {
           description="I specialize in backend technologies like Python (Flask), Node.js, and Laravel. From Websockets to Task Scheduling, I deliver robust server-side solutions. Familiar with RESTful APIs, GraphQL, and database management (PostgreSQL, MongoDB)."
         />
         <Card
-          icon={<GitHubLogoIcon height={40} width={40} />}
-          title="CI/CD"
-          descriptionHeader="Currently learning CI/CD"
-          description="Currently learning continuous integration and continuous deployment practices to automate the software delivery process using GitHub-Actions. I am currently experimenting with my own little Hostinger VPS."
+          icon={<GoalIcon height={40} width={40} />}
+          title="Goals"
+          descriptionHeader="Things I want to get better at"
+          description="Currently learning continuous integration and continuous deployment practices to automate the software delivery process using GitHub-Actions. I am currently experimenting with my own little Hostinger VPS. I also want to level up my skills in Docker and Kubernetes. Last but not least, I am always trying to improve my skills in JavaScript, CSS, especially CSS, because there is always something new to learn, and for me, it is hard to master. In general, I am always trying to improve my code🙂"
         />
       </Spotlight>
     </section>
